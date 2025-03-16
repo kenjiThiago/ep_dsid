77eb10fcac7dca8785c8b05190459c9ad7dc8f06
Thiago Kenji <thiago.kenji123@gmail.com>
2025-03-16 20:34:36 -0300

    [ep] Mudado o input do eachare.py.

--- eachare.py
+++ eachare.py
@@ -16,7 +16,7 @@ thread_servidor = threading.Thread(target = peer.inicia_servidor, daemon = True)
 thread_servidor.start()
 
 while True:
-    comando = int(input('''Escolha um comando:
+    comando = input('''Escolha um comando:
         [1] Listar peers
         [2] Obter peers
         [3] Listar arquivos locais
@@ -24,18 +24,18 @@ while True:
         [5] Exibir estatisticas
         [6] Alterar tamanho de chunk
         [9] Sair
-> '''))
+> ''')
     print()
 
     match comando:
-        case 1:
+        case "1":
             peer.lista_peers()
-        case 2:
+        case "2":
             peer.obter_peers()
-        case 3:
+        case "3":
             peer.lista_arquivos_locais()
-        case 9:
+        case "9":
             peer.sair()
             break
         case _:
-            print("Comando não conhecido")
+            print("Comando não conhecido\n")
